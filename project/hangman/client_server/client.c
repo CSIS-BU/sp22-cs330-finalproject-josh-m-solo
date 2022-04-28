e57cd1f8d1d986f7e7b73ba0d16f935b951aed2c
Joshua McGrew <jjmcgrew@mail.bradley.edu>
2022-04-27 23:40:46 -0500

    updates
    code rewritten/reorganized, game working

--- project/hangman/client_server/client.c
+++ project/hangman/client_server/client.c
@@ -14,11 +14,46 @@
 
 #define BUFFER_SIZE 2048
 
+void printHangmanImage(int guesses)
+{
+	char* lOne = "  * * * \n *     *\n *     *\n  * * *";
+	char* lTwo = "  * * * \n *     *\n *     *\n  * * *\n    *\n    *\n    *\n    *";
+	char* lThree = "  * * * \n *     *\n *     *\n  * * *\n    *\n* * *\n    *\n    *";
+	char* lFour = "  * * * \n *     *\n *     *\n  * * *\n    *\n* * * * *\n    *\n    *";
+	char* lFive = "  * * * \n *     *\n *     *\n  * * *\n    *\n* * * * *\n    *\n    *\n   *\n  *\n *";
+	char* lSix = "  * * * \n * x x *\n * ___ *\n  * * *\n    *\n* * * * *\n    *\n    *\n   * *\n  *   *\n *     *";
+
+	switch (guesses)
+	{
+	case 5:
+		printf("\n%s", lOne);
+		break;
+	case 4:
+		printf("\n%s", lTwo);
+		break;
+	case 3:
+		printf("\n%s", lThree);
+		break;
+	case 2:
+		printf("\n%s", lFour);
+		break;
+	case 1:
+		printf("\n%s", lFive);
+		break;
+	case 0:
+		printf("\n%s", lSix);
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
+
 //client function
 int client(char* server_ip, char* server_port)
 {
 	//variable declaration
-	int sock_fd; //file descriptor
+	int sock_fd;
 	struct addrinfo hints, * server_info, * p;
 	socklen_t sin_size;
 	char buffer[BUFFER_SIZE] = { 0 };
@@ -27,145 +62,164 @@ int client(char* server_ip, char* server_port)
 	char* status;
 
 	//hangman variables
-	int playAgainInput = 1;
+	int playAgainInput;
+	const int MAX_GUESSES = 6;
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
 	error = getaddrinfo(server_ip, server_port, &hints, &server_info);
-	if (error)
+
+	//print instructions
+	printf("Welcome to Hangman, a word guessing game.\nEach game, a new word will be chosen and you must guess letters in the word one at a time.\nYou lose if you guess incorrectly 6 times.\nYou win if you guess the entire word.\n\nType [1] to play a new game or [0] to quit: ");
+	scanf("%d", &playAgainInput);
+	while ((getchar()) != '\n');
+
+	while (1)
 	{
-		//errx(1, "&s", gai_strerror(error));
-		perror("Client: connect");
+		if (playAgainInput != 0 && playAgainInput != 1)
+		{
+			printf("Enter [0] or [1]: ");
+			scanf("%d", &playAgainInput);
+			while ((getchar()) != '\n');
+		}
+		else { break; }
 	}
 
-	//create socket
-	if ((sock_fd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol)) < 0)
-	{
-		perror("Client: socket");
-		exit(-1);
-	}
-
-	//connect to server
-	if (connect(sock_fd, server_info->ai_addr, server_info->ai_addrlen) < 0)
-	{
-		perror("Client: connect");
-		exit(-1);
-	}
-
-	//CLIENT GAME CODE HERE
-
-	//RECEIVE instructions from server and print
-	//if (recv(sock_fd, recc, sizeof(recc), 0) < 0)
-	//{
-	//	perror("Client: receive failed (instructions)");
-	//}
-	recv(sock_fd, buffer, BUFFER_SIZE, 0);
-	printf("%s", buffer);
-	memset(buffer, 0, strlen(buffer));
-	printf("Empty buffer?: %s", buffer);
 	//enter game loop
 	while (playAgainInput == 1)
 	{
-		break;
+		//create socket
+		if ((sock_fd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol)) < 0)
+		{
+			perror("Client: socket");
+			exit(-1);
+		}
+
+		//connect to server
+		if (connect(sock_fd, server_info->ai_addr, server_info->ai_addrlen) < 0)
+		{
+			perror("Client: connect");
+			exit(-1);
+		}
+
+		//CLIENT GAME CODE HERE
+
+		//receive the word from the server
+		if (recv(sock_fd, buffer, BUFFER_SIZE, 0) < 0)
+		{
+			perror("Client: receive failed");
+		}
+
+		//set word
+		const char* word = buffer;
+
 		int wordLength = 0;
-		int guesses = 0;
+		int guesses = MAX_GUESSES;
 		char lettersGuessed[32];
 		char wordInProgress[16];
-		char word[16];
-		char letter; //the guessed letter
-		//info variables
-		int info1 = 0;
-		int info2 = 0;
-		int info3 = 0;
-		
-		//empty the strings
+		char letter;
+		char* image;
 		memset(wordInProgress, 0, sizeof(wordInProgress));
 		memset(lettersGuessed, 0, sizeof(lettersGuessed));
-		memset(word, 0, sizeof(word));
+		//find length of the word
+		for (wordLength = 0; word[wordLength] != '\0'; wordLength++) {}
 
+		printf("\nYour new word has been chosen.");
 
-		//RECEIVE wordlength
-
-		//enter secondary loop (guesses)
+		//intitialize wordInProgress to blank spaces based on word length
+		for (int i = 0; i < wordLength; i++)
+		{
+			wordInProgress[i] = '-';
+		}
+		//printf("INPUT: %d\nWORD: %s\nLENGTH: %d\nWIP: %s", playAgainInput, word, wordLength, wordInProgress);
+		//enter the guesses loop
 		while (1)
 		{
-			//RECEIVE information:
-			//Receiving 'guesses' to display guesses remaining
-			//Receiving 'lettersGuessed' for already guessed letters
-			//Receiving 'wordInProgress' to display	
-
 			//print the guess prompt
-			printf("\n\n***\nWrong Guesses Remaining: %i\n", guesses);
+			printHangmanImage(guesses);
+			printf("\n\n# # #\n\nWrong Guesses Remaining: %i\n", guesses);
 			printf("You have already guessed: %s\n", lettersGuessed);
 			printf("Word: %s\n", wordInProgress);
+			printf("The word has %d letters.\n", wordLength);
 			printf("Guess the next letter: ");
-			//get input and store in 'letter'
-			//SEND the letter to the server
+			//get next guess
+			scanf("%c", &letter);
+			while ((getchar()) != '\n');
 
-			//next the client receives information.
-			//info1 refers to if the letter has been guessed already.
-			//note that info2 and info3 are bypassed if info1 indicates that a letter has been guessed already
-			
-			//RECEIVE info1 (0 indicates letter that hasn't been guessed, 1 indicates letter has been guessed.)
-			if (info1 == 1)
+			//check if letter has already been guessed
+			if (strchr(lettersGuessed, letter) == NULL)
 			{
-				//this guess is over
-				printf("\n%c has been guessed already.", letter);
-			}
-			else
-			{
-				//server now processes the guess.
-				//next, info2 refers to whether the guess is correct (0) or incorrect (1)
-				//RECEIVE info2
-				if (info2 == 0)
+				//letter has not been guessed yet
+				//check if letter is in the word
+				if (strchr(word, letter) != NULL)
 				{
-					printf("\nThat guess is correct!");
+					//letter is in the word
+					//build display word with new letter(s)
+					printf("\nThat guess is correct!\n");
+					for (int j = 0; j < wordLength; j++)
+					{
+						if (word[j] == letter)
+						{
+							wordInProgress[j] = letter;
+						}
+					}
 				}
 				else
 				{
-					printf("%c is not in the word. You have lost a guess!", letter);
+					//letter is not in the word, decrement guesses
+					printf("\n%c is not in the word. You have lost a guess!\n", letter);
+					guesses--;
 				}
-
-				//info3 has 3 possible values:
-				//game is not over (0) (nothing is printed in this case)
-				//game is over and player won (1)
-				//game is over and player lost (2)
-				//RECEIVE info3
-				if (info3 == 1)
-				{
-					printf("\nYou have guessed the word!");
-					break;
-				}
-				else if (info3 == 2)
-				{
-					printf("\nYou have run out of guesses! You could not guess the word!");
-					break;
-				}
-				//loops back if break does not occur
+				//add letter to guessed letters
+				strncat(lettersGuessed, &letter, 1);
+			}
+			else
+			{
+				//letter has been guessed already
+				printf("\n%c has been guessed already.\n", letter);
+			}
+			//check if guesses have run out
+			if (guesses <= 0)
+			{
+				printf("\nYou have run out of guesses! You could not guess the word!\n");
+				printHangmanImage(guesses);
+				break;
+			}
+			//also check if word is completed
+			if (strcmp(wordInProgress, word) == 0)
+			{
+				printf("\nYou have guessed the word!");
+				break;
 			}
 		}
-		//RECEIVE word from server and reveal it (regardless of whether you won or lost)
-		printf("\nGame Over! The word was: %s", word);
-		printf("\nWould you like to play again? (1: Yes, 0: No)");
-		//get input to playAgainInput
-		//SEND playAgain response, playAgain also determines if client loops again too
+		//game is over, reveal word
+		printf("\nGame Over! The word was: %s\n\nWould you like to play again? [1] Yes, [0] No: ", word);
+		scanf("%d", &playAgainInput);
+		while ((getchar()) != '\n');
+		while (1)
+		{
+			if (playAgainInput != 0 && playAgainInput != 1)
+			{
+				printf("Enter [0] or [1]: ");
+				scanf("%d", &playAgainInput);
+				while ((getchar()) != '\n');
+			}
+			else { break; }
+		}
+		close(sock_fd);
 	}
-
-	//done
-	close(sock_fd);
 	return 0;
 }
 
 //main function
 int main(int argc, char* argv[])
 {
-
 	char* server_ip;
 	char* server_port;
 
-	//validate 
+	//validate
 	if (argc != 3)
 	{
 		fprintf(stderr, "Client usage: ./client [server IP] [server port]\n");
--- project/hangman/client_server/server.c
+++ project/hangman/client_server/server.c
@@ -20,26 +20,19 @@
 int server(char* server_port)
 {
 	//socket variables declaration
-	char buffer[BUFFER_SIZE] = { 0 };
+	//char buffer[BUFFER_SIZE] = { 0 };
 	int sock_fd, new_sock_fd;
 	struct addrinfo hints, * serv_info;
 	struct sockaddr_in client_addr;
 	socklen_t length;
 	int error;
 	int yes = 1;
-	FILE* fp;
+	int buf_len;
 
-	//declare hangman-related variables
-	const int HANGMAN_WORD_COUNT = 34; //34 words in test, 124 words in hangman_words.txt
-	const int MAX_GUESSES = 6;
-	//int counter = 0;
-	//const char* fileName = "hangman_words.txt";
-	//const char* words[HANGMAN_WORD_COUNT - 1];
+	//declare hangman related variables
+	const int HANGMAN_WORD_COUNT = 34;
 	const char* words[] = { "birthday", "camera", "challenge", "creation", "database", "emotion", "energy", "foundation", "generator", "hardware", "homework", "inspector", "jacket", "kingdom", "library", "manager", "morning", "nightmare", "office", "packet", "payment", "protocol", "resources", "router", "science", "session", "software", "switch", "technology", "thunder", "vehicle", "winter", "yogurt" };
-	//strings to be used in the game
-	char* instructions = "Welcome to Hangman, a word guessing game.\nEach game, a new word will be chosen and you must guess letters in the word one at a time.\nYou lose if you guess incorrectly 6 times.\nYou win if you guess the entire word.\n\nType [1] to play a new game or [0] to quit.";
-	int playAgain = 1;
-	//seed rng
+	
 	srand(time(0));
 
 	//build address data structure
@@ -48,23 +41,7 @@ int server(char* server_port)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	//open file (hangman_words.txt) and put into words[] array
-	//fp = fopen(fileName, "r");
-	//fread(words, 16, 1, fp);
-	//while (fscanf(fp, "%s", words[counter]) != EOF)
-	//{
-	//	counter++;
-	//}
-	//close file
-	//fclose(fp);
-
-
 	error = getaddrinfo(NULL, server_port, &hints, &serv_info);
-	if (error)
-	{
-		//errx(1, "%s", gai_strerror(error));
-		perror("Server: getaddrinfo");
-	}
 
 	//create socket
 	if ((sock_fd = socket(serv_info->ai_family, serv_info->ai_socktype, serv_info->ai_protocol)) < 0)
@@ -100,130 +77,26 @@ int server(char* server_port)
 		perror("Server: listen error");
 		exit(-1);
 	}
+
 	//wait for connection
 	while (1)
 	{
-		memset(buffer, 0, sizeof(buffer));
 		length = sizeof client_addr;
 		if ((new_sock_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &length)) < 0)
 		{
 			perror("Server: accept error");
 			exit(-1);
 		}
-		printf("SERVER: I AM CONNECTED. ABOUT TO SEND.\n");
 
-		//GAME CODE HERE
-
-		//SEND instructions
-		if (send(new_sock_fd, instructions, strlen(instructions), 0) < 0)
-		{
-			perror("Server: send failed (instructions)");
-		}
-		printf("SERVER: SENT\n");
-		break;
-		//RECEIVE integer response to start new game
-		if (recv(sock_fd, buffer, sizeof(playAgain), 0) < 0)
-		{
-			perror("Server: receive failed (response to instructions)");
-		}
-		playAgain = buffer[0];
-		memset(buffer, 0, sizeof(buffer));
+		//Game code
+		//generate random number and assign word
+		int word_number = rand() % HANGMAN_WORD_COUNT;
+		const char* word = words[word_number];
 		
-		//loop won't run if player doesn't start the game
-
-		while (playAgain == 1)
+		//SEND the word to the client
+		if (send(new_sock_fd, word, strlen(word), 0) < 0)
 		{
-			//generate random number and assign new word
-			int word_number = rand() % HANGMAN_WORD_COUNT;
-			const char* word = words[word_number];
-
-			int wordLength = 0;
-			int guesses = MAX_GUESSES; //guesses remaining
-			char lettersGuessed[32]; //keep track of guessed letters
-			char letter; //the guessed letter
-			char wordInProgress[16]; //for the in-progress word, it's reset each time the loop runs
-
-			//find length of the word
-			for (wordLength = 0; word[wordLength] != '\0'; wordLength++) {}
-			//Initial SEND wordLength to client, may not need?
-
-			memset(wordInProgress, 0, sizeof(wordInProgress));
-			memset(lettersGuessed, 0, sizeof(lettersGuessed));
-			//intitialize wordInProgress to blank lines for the word length
-			for (int i = 0; i < wordLength; i++)
-			{
-				wordInProgress[i] = '-';
-			}
-			//note, word selection, word length, and word in progress are all functional
-
-			//enter the guesses loop
-			while (1)
-			{
-				//SEND information to client, client will build the prompt from there
-				//Sending 'guesses' for guesses remaining
-				//Sending 'lettersGuessed' for already guessed letters
-				//Sending 'wordInProgress' to be displayed
-
-				//RECEIVE the letter and store it in 'letter'
-				//check if letter has already been guessed
-				if (strchr(lettersGuessed, letter) != NULL)
-				{
-					//letter has been guessed already
-					//SEND info that the letter has been guessed already (info1 = 1)
-					//this guess loop is over
-				}
-				else
-				{
-					//SEND info that the letter hasn't been guessed yet (info1 = 0)
-
-					//if time permits, check here if character is valid (lowercase letter only)
-					//if it isn't valid, end the guess
-					
-					//check if letter is in the word
-					if (strchr(word, letter) != NULL)
-					{
-						//letter is in the word
-						//SEND info that guess is correct (info2 = 0)
-						//build  display word with new letter(s) in it Ex: "_ E _ _ _"
-						for (int j = 0; j < wordLength; j++)
-						{
-							if (word[j] == letter)
-							{
-								wordInProgress[j] = letter;
-							}
-						}
-					}
-					else
-					{
-						//letter is not in the word, decrement guesses
-						guesses--;
-						//SEND info that guess is incorrect (info2 = 1)
-					}
-
-					//guess complete, check if guesses have run out
-					if (guesses <= 0)
-					{
-						//SEND info that game is over (lost) (info3 = 2)
-						break;
-					}
-					//also check if word is completed
-					//strcmp??
-					if (strcmp(wordInProgress, word) == 0) //may not work?
-					{
-						//word is completed.
-						//SEND info that game is over (won) (info3 = 1)
-						break;
-					}
-
-					//SEND info that game is not over (info3 = 0)
-				}
-
-			}
-			//current game is over
-			//SEND word to client for reveal
-			//RECEIVE play again response (1 sets playAgain to true, 0 sets playAgain to false)
-			//loop will end if player decides not to play again
-			break;
+			perror("Server: send failed");
 		}
 
 		//done
@@ -236,7 +109,7 @@ int server(char* server_port)
 int main(int argc, char* argv[])
 {
 	char* server_port;
-	
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Server usage: ./server [server port]\n");
